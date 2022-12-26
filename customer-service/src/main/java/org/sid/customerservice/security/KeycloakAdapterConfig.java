396bb4086ae7f8e4118950b8c510c362dafaedd9
Yassamine-sahl <yassamine.sahl@gmail.com>
2022-12-26 19:33:07 +0100

    Micro-Service Architecture : Customer Service with Keycloak

--- customer-service/src/main/java/org/sid/customerservice/security/KeycloakAdapterConfig.java
+++ customer-service/src/main/java/org/sid/customerservice/security/KeycloakAdapterConfig.java
@@ -6,12 +6,16 @@ import org.keycloak.adapters.springsecurity.client.KeycloakRestTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@Configuration  //=> il permet de configurer la sécurité de l'application
 public class KeycloakAdapterConfig {
     @Bean
-    public KeycloakSpringBootConfigResolver keycloakConfigResolver(){
+    public KeycloakSpringBootConfigResolver keycloakConfigResolver(){   //=> il permet de dire au keycloak de lire le fichier application.properties
         return new KeycloakSpringBootConfigResolver();
-
+        //=> tu vas se baser sur le fichier application.properties pour configurer keycloak et pas keycloak.json
     }
 
+    /*@Bean
+        KeycloakRestTemplate keycloakRestTemplate(KeycloakClientRequestFactory keycloakClientRequestFactory){
+        return new KeycloakRestTemplate(keycloakClientRequestFactory);
+    }*/
 }
